e1d6d3c13a002df2cc8106f0e99b4d1cd35dbbe8
Romaisa Ayoub <romaisaayob@gmail.com>
2026-02-19 23:20:04 +0600

    Add files via upload

---
+++ Array simple.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+ string M[4]={"Romaisa","Farhan","Shahmeer","Arooj"};
+cout << M[0] << endl;
+cout << M[1] << endl;
+cout << M[2] << endl;
+M[3]="Fatima";
+cout << M[3] << endl;}
