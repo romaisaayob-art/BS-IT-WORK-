120d3f0fe43510e1d4eb97784f41620053a46fc9
Romaisa Ayoub <romaisaayob@gmail.com>
2026-02-24 00:12:21 +0600

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void asim(){
+int a, b;
+cout << "Enter the values" << endl;
+cin >> a>>b;
+if(a<b){
+cout << "Condition is true" << endl;}}
+void ahsan(){
+cout << "This function is for Ahsan" << endl;
+}
+int main(){
+asim();
+ahsan();}
