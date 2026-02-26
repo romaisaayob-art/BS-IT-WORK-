3f5b5122d93bfd8e78f3a95cd42bf0755e28b413
Romaisa Ayoub <romaisaayob@gmail.com>
2026-02-27 00:58:38 +0600

    Semester 2/

---
+++ Oop Basic program .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Myclass{
+public:
+int age;
+string name;};
+
+int main()
+{Myclass myobject;
+myobject.age=12;
+myobject.name="Romaisa";
+cout << "My detail😁" << endl;
+cout <<"Age="<< myobject.age ;
+cout << "\t Name="<<myobject.name;
+    return 0;
+    }
+    
