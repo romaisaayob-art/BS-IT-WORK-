10d0edc6cd92b28511477ca34bf90f04a07bd33b
Romaisa Ayoub <romaisaayob@gmail.com>
2026-03-08 23:41:11 +0600

    Add files via upload

---
+++ Semester/oop book.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Book{
+public:
+string title;
+string author;
+int price;};
+int main(){
+Book s1;
+s1.title="Peere kamil";
+s1.author="Umerah Ahmad";
+s1.price=1000;
+cout << "Title:" <<s1.title<<endl;
+cout << "Author:" <<s1.author<<endl;
+cout << "Price:" <<s1.price<<endl;
+}
+
+    
